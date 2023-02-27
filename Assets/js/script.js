b4c2fd0b2035a55a1fb4cc46e28d97a87f88beac
thomasjshea <thomasjshea89@gmail.com>
2023-02-27 14:59:20 -0600

    added date to top of page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,10 +14,13 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  var currentTime = dayjs().hour()
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDay = dayjs().format('dddd, MMMM D')
+  $('#currentDay').text(currentDay)
 });
